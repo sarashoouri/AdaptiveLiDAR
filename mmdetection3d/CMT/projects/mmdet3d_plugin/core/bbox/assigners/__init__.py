7c7dc1d09ac4f751adeb45136a683f9e7a3f5542
Sara Shoouri <64006343+sarashoouri@users.noreply.github.com>
2026-02-03 19:35:24 -0500

    Add files via upload

---
+++ mmdetection3d/CMT/projects/mmdet3d_plugin/core/bbox/assigners/__init__.py
@@ -0,0 +1,4 @@
+from .hungarian_assigner_3d import HungarianAssigner3D
+from .hungarian_assigner_QT import HungarianAssigner3D_QT
+#__all__ = ['HungarianAssigner3D']
+__all__ = ['HungarianAssigner3D', 'HungarianAssigner3D_QT']
