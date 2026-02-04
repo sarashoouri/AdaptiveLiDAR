00bd2cfd76c1b3e6705e1b3da7ee545081758bad
Sara Shoouri <64006343+sarashoouri@users.noreply.github.com>
2026-02-03 19:39:06 -0500

    Delete mmdetection3d/CMT/projects/mmdet3d_plugin/core/coders directory

--- mmdetection3d/CMT/projects/mmdet3d_plugin/core/coders/__init__.py
+++
@@ -1,4 +0,0 @@
-#from .multi_task_bbox_coder_lyft import MultiTaskBBoxCoder # For Lyft
-from .multi_task_bbox_coder import MultiTaskBBoxCoder #For Nuscenes
-from .transfusion_bbox_coder import TransFusionBBoxCoder
-__all__ = ['MultiTaskBBoxCoder', 'TransFusionBBoxCoder']
